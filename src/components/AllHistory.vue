7b23edf009a5c8f31ab2835491e58f500e20f19c
momoko047 <fizz96500@gmail.com>
2019-10-19 19:32:58 +0900

    allhistory Devicelist の追加

---
+++ src/components/AllHistory.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="history">
+  <table>
+   <tr>
+    <th>デバイス名</th>
+    <th>貸出状況</th>
+    <th>メモ</th>
+    <th>削除</th>
+  </tr>
+  <tr>
+    <td>USB1</td>
+    <td>貸出中</td>
+    <td>10/19 20:30</td>
+  </tr>
+  <tr>
+    <td>さくら</td>
+    <td>充電気２</td>
+    <td>10/19 18:10</td>
+    <td>10/19 19:00</td>
+    <td>10/19 18:30</td>
+  </tr>
+  <tr>
+    <td>さくら</td>
+    <td>充電気２</td>
+    <td>10/19 18:10</td>
+    <td>10/19 19:00</td>
+    <td>10/19 18:30</td>
+  </tr>
+  <tr>
+    <td>さくら</td>
+    <td>充電気２</td>
+    <td>10/19 18:10</td>
+    <td>10/19 19:00</td>
+    <td>10/19 18:30</td>
+  </tr>
+  <tr>
+    <td>さくら</td>
+    <td>充電気２</td>
+    <td>10/19 18:10</td>
+    <td>10/19 19:00</td>
+    <td>10/19 18:30</td>
+  </tr>
+  <tr>
+    <td>さくら</td>
+    <td>充電気２</td>
+    <td>10/19 18:10</td>
+    <td>10/19 19:00</td>
+    <td>10/19 18:30</td>
+  </tr>
+  <tr>
+    <td>さくら</td>
+    <td>充電気２</td>
+    <td>10/19 18:10</td>
+    <td>10/19 19:00</td>
+    <td>10/19 18:30</td>
+  </tr>
+</table>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Aside',
+}
+</script>
+
+
+<style scoped>
+.history{
+  padding-top: 100px;
+}
+
+</style>
