2051eb53fa0fb7ed2dea36befe6a79ca73f7ef43
vagrant <vagrant@localhost.localdomain>
2019-10-19 07:33:14 +0000

    QRコードリーダー読み込み確認しました

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Dashbord from './components/Dashbord'
 import Users from './components/Users'
+import QRcode_reader from './components/QRcode_reader'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
             path: '/users',
             name: 'Users',
             component: Users
+        },
+        {
+            path: '/QRcode_reader',
+            name: 'QRcode_reader',
+            component: QRcode_reader
         }
 
     ]
