726ea35c18241cf32a70e82375018d650d3af294
Azuma926 <okarinasoujirou@gmail.com>
2019-10-19 10:44:00 +0000

    table

--- src/components/QRcodeReader.vue
+++ src/components/QRcodeReader.vue
@@ -2,66 +2,73 @@
   <div class>
     <p class="error">{{ error }}</p>
 
-    <p class="decode-result">Last result: <b>{{ result }}</b></p>
-
-    <ul>
-      <li v-for="item in list" :key="item.id">
-        <b>{{item}}</b>
-      </li>
-    </ul>
-
     <qrcode-stream class="QRcodeReader" @decode="onDecode" @init="onInit" />
+
+    <table>
+      <thead>
+        <tr>
+          <th>デバイス名</th>
+          <th>×</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list" :key="item.id">
+          <td>{{item}}</td>
+          <td>
+            <button type="button" class="btn btn-danger">削除</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import { QrcodeStream } from 'vue-qrcode-reader'
+import { QrcodeStream } from "vue-qrcode-reader";
 
 export default {
-
   components: { QrcodeStream },
 
-  data () {
+  data() {
     return {
-      result: '',
+      result: "",
       list: [],
-      error: ''
-    }
+      error: ""
+    };
   },
 
   methods: {
-    onDecode (result) {
-      this.result = result
-      const flag = this.list.some(value => value == result)
-      if(!flag){
-        this.list.push(result)
-      }
-      else{
-        alert("読み込み済み")
+    onDecode(result) {
+      this.result = result;
+      const flag = this.list.some(value => value == result);
+      if (!flag) {
+        this.list.push(result);
+      } else {
+        alert("読み込み済み");
       }
     },
 
-    async onInit (promise) {
+    async onInit(promise) {
       try {
-        await promise
+        await promise;
       } catch (error) {
-        if (error.name === 'NotAllowedError') {
-          this.error = "ERROR: you need to grant camera access permisson"
-        } else if (error.name === 'NotFoundError') {
-          this.error = "ERROR: no camera on this device"
-        } else if (error.name === 'NotSupportedError') {
-          this.error = "ERROR: secure context required (HTTPS, localhost)"
-        } else if (error.name === 'NotReadableError') {
-          this.error = "ERROR: is the camera already in use?"
-        } else if (error.name === 'OverconstrainedError') {
-          this.error = "ERROR: installed cameras are not suitable"
-        } else if (error.name === 'StreamApiNotSupportedError') {
-          this.error = "ERROR: Stream API is not supported in this browser"
+        if (error.name === "NotAllowedError") {
+          this.error = "ERROR: you need to grant camera access permisson";
+        } else if (error.name === "NotFoundError") {
+          this.error = "ERROR: no camera on this device";
+        } else if (error.name === "NotSupportedError") {
+          this.error = "ERROR: secure context required (HTTPS, localhost)";
+        } else if (error.name === "NotReadableError") {
+          this.error = "ERROR: is the camera already in use?";
+        } else if (error.name === "OverconstrainedError") {
+          this.error = "ERROR: installed cameras are not suitable";
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.error = "ERROR: Stream API is not supported in this browser";
         }
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
